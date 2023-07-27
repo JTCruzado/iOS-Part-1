ee7d5403bdc4c6b8f5e5a4f33a13767f9fc5d488
Evan Klein <evanrklein@gmail.com>
2023-07-27 16:32:43 -0400

    finished and converted to theme
    now has a theme of pastries/deserts

--- lab-files/lab-8/lab-8.playground/Contents.swift
+++ lab-files/lab-8/lab-8.playground/Contents.swift
@@ -1,28 +1,7 @@
-import UIKit
-
 /*
  
 Day 8 - Optionals, Protocols, and Extensions
  
-*/
-
-print("\n💡 Topic 1 💡\n TEXT \n")
-
-// Base template of class
-
-
-//-------------------------------------------------------------------------------------------
-print("💡 Topic 1A 💡\n TEXT \n")
-
-
-
-//-------------------------------------------------------------------------------------------
-
-print("\n🧠 Challenge 1A: 🧠")
-print("MORE TEXT👇\n")
-
-/*
- 👇 Your new code below 👇
 */
 
 
@@ -30,38 +9,38 @@ print("\n💡 Topic 1 💡\n Missing Data \n")
 
 // In Swift, there is a datatype that can either hold a value or none at all, the optional:
 
-// here phoneNumberField is holding a value,
+// here numCookieOrders is holding a value,
 
-var phoneNumberField: Int? = 1234567890
+var numCookieOrders: Int? = 15
 
 // but the "?" after the String type means that this variable can hold nothing:
-phoneNumberField = nil
+numCookieOrders = nil
 
 // nil means nothing. There is no information in this variable
 
 // one way to check for nil values is to use if let statements:
 
-if let numberField = phoneNumberField {
+if let cookieOrders = numCookieOrders {
     // if numberField is not nil
-    print(numberField)
+    print(cookieOrders)
 } else {
     // if numberField is nil
-    print("nothing!")
+    print("no cookie orders!")
 }
 
 // if you know there will always be a value in an optional, you can unwrap it by putting "!" afterwards
 
-phoneNumberField = 5505550550 // there's always a value here because we just set it
+numCookieOrders = 5505550550 // there's always a value here because we just set it
 
-print(phoneNumberField) // prints Optional(5505550550) because it's not unwrapped
-print(phoneNumberField!) // prints 5505550550 because it's unwrapped now
+print(numCookieOrders) // prints Optional(5505550550) because it's not unwrapped
+print(numCookieOrders!) // prints 5505550550 because it's unwrapped now
 
 //-------------------------------------------------------------------------------------------
 
 print("\n🧠 Challenge 1: 🧠")
 print("Handle the return value of this function. Use an if let statement, if it returns nothing, say something, else, return the value👇\n")
 
-func randomTrueFalse() -> Int? {
+func wantsCookies() -> Int? {
     let randomValue = Int.random(in: 0...2)
     if randomValue == 1{
         return nil
@@ -80,28 +59,32 @@ print("\n💡 Topic 2 💡\n Guard \n")
 // it looks similar to the if let statement:
 
 var emptySignUpForm: [String: String] = [:]
-var namedForm: [String: String] = ["name": "Hailey"]
+var namedForm: [String: String] = ["name": "Aiden", "favoriteSweet": "Cookies"]
 
-func helloFromForm(form: [String: String]) {
-    guard let name = form["name"] else {
-        print("no name in form")
+func getFavoriteDesert(form: [String: String]) {
+    guard let desert = form["favoriteSweet"] else {
+        print("data missing")
         return
     }
-    print("hello \(name)!")
+    guard let name = form["name"] else {
+        print("data missing")
+    }
+    print("\(name)'s favorite desert is \(desert)")
 }
 
-getName(form: emptySignUpForm) // prints "no name in form"
-getName(form: namedForm) // prints "hello Hailey!"
+getFavoriteDesert(form: emptySignUpForm) // prints "no name in form"
+getFavoriteDesert(form: namedForm) // prints "Aiden's favorite desert is Cookies!"
 
 //-------------------------------------------------------------------------------------------
 
 print("\n🧠 Challenge 2: 🧠")
-print("Make a function that takes two numbers and prints the sum of their squares. If either of them is missing, break out of the function and print something notifying the user 👇\n")
+print("Print the sum of the sugarGrams and the flour grams in this function using the guard function.👇\n")
 
-/*
- 👇 Your new code below 👇
-*/
-
+// complete the function
+func bakePastry(sugarGrams: Int?, flourGrams: Int?) {
+    // Make sure both sugarGrams and flourGrams are not nil
+    // If either sugarGrams or flourGrams is nil, print something to the user
+}
 
 print("\n💡 Topic 3 💡\n Nil Coalescing \n")
 
@@ -111,9 +94,9 @@ let userInput: Bool? = nil
 
 // you probably don't want to continue if the user puts nothing, so use nil coalescing to default to false if that happens
 // to use it, place the possibly nil value with a ?? and then the default value:
-let shouldContinue = userInput ?? false
+let wantsMoreCake = userInput ?? false
 
-print(shouldContinue) // prints false
+print(wantsMoreCake) // prints false
 
 //-------------------------------------------------------------------------------------------
 
@@ -139,64 +122,56 @@ print("\n💡 Topic 4 💡\n Optional Chaining \n")
 
 // optional chaining makes sure that if you try to get the property of something that is nil, it will just return nil and not break your code
 
-struct Person {
-    var house: House?
+struct Pastry {
+    let name: String
+    var bakery: Bakery?
 }
 
-struct House {
-    var numRooms: Int = 1
+struct Bakery {
+    var numBakers: Int = 10
 }
 
-var jerry = Person()
-if let numRooms = jerry.house?.numPeople { // because you have nil here, you can use an if let statement or other forms of Optional code on it
-    print("there are \(numRooms) rooms in the house")
+var oreo = Pastry(name: "Oreo")
+if let numBakers = oreo.bakery?.numBakers { // because you have nil here, you can use an if let statement or other forms of Optional code on it
+    print("there are \(numBakers) rooms in the bakery")
 } else {
-    print("there are an unknown amount of rooms in the house")å
+    print("there are an unknown amount of bakers where the cookie was made")
 }
 
 //-------------------------------------------------------------------------------------------
-
-print("\n🧠 Challenge 4: 🧠")
-print("  👇\n")
-
-
-
-/*
- 👇 Your new code below 👇
-*/
 
 print("\n💡 Topic 5 💡\n Function Faliure \n")
 
 // if you want to have a default value in case of a function throwing an error, you can use the "try?" keyword
 
 // ignore this
-enum NoEmailError: Error { // makes a specific error for this example
-    case noEmail
+enum NoIngredientError: Error { // makes a specific error for this example
+    case noIngredient
 }
 
-// function to "get" the user email
-func getUserEmail(username: String) throws -> String {
-    throw NoEmailError.noEmail
+// function to "get" the ingredient
+func getIngredient(name: String) throws -> String {
+    throw NoIngredientError.noIngredient
 }
 
-// if the user's email exists, there will be no error and the email will be printed out
-// if there is, the "try?" part will instead return nil and the if let statement handles it, printing "this user's email does not exist" instead
-if let email = try? getUserEmail(username: "thealignedmind") {
-    print(email)
+// if the ingredient exists, there will be no error and the ingredient will be printed out
+// if there is, the "try?" part will instead return nil and the if let statement handles it, printing "this ingredient does not exist" instead
+if let ingredient = try? getIngredient(name: "sugar") {
+    print(ingredient)
 } else {
-    print("this user's email does not exist")
+    print("this ingredient does not exist")
 }
 
 print("\n🧠 Challenge 5: 🧠")
 print("Use the return value from this function and handle the error with the try? statement and nil coalescing👇\n")
 
 // ignore this
-enum NoNameError: Error {
-    case noName
+enum NoPastryError: Error {
+    case noPastry
 }
 
-func getNameFromPhoneNumber(phoneNumber: Int) throws -> String {
-    throw NoNameError.noName
+func getPastryFromIngredient(ingredient: String) throws -> String {
+    throw NoPastryError.noPastry
 }
 
 /*
@@ -207,58 +182,104 @@ print("\n💡 Topic 6 💡\n Protocols \n")
 
 // A protocol is a blueprint for the functionality of whatever conforms to it.
 // Protocols don't have any functionality inside them, but they define the types and names of the properties and the name and input types and output types of methods
-protocol Vehicle {
-    // protocol properties:
+protocol PastryProtocol {
     var name: String { get }
-    var numPassengers: Int { get set }
+    var numServings: Int { get set }
     
-    // protocol methods:
-    func isAtMaxSpeed(currentSpeed: Int) -> Bool
-    func travel(distance: Int) -> Void
+    func isBaked() -> Bool
+    mutating func consume() -> Void
 }
 
-// with this protocol you can make structs/classes that conform to it:
-
-struct Car: Vehicle {
+struct Cake: PastryProtocol {
     let name: String
-    var numPassengers: Int = 5
+    var numServings: Int = 8
     
-    func isAtMaxSpeed(currentSpeed: Int) -> Bool {
-        return currentSpeed == 100
+    func isBaked() -> Bool {
+        return true
     }
     
-    func travel(distance: Int) {
-        print("driving \(distance) miles!")
+    mutating func consume() {
+        print("Eating a slice of \(name)!")
+        numServings -= 1
     }
 }
 
-
-struct Bike: Vehicle {
+struct Doughnut: PastryProtocol {
     let name: String
-    var numPassengers: Int = 1
+    var numServings: Int = 1
     
-
-    func isAtMaxSpeed(currentSpeed: Int) -> Bool {
-        return currentSpeed == 30
+    func isBaked() -> Bool {
+        return false
     }
     
-    func travel(distance: Int) {
-        print("biking \(distance) miles!")
+    mutating func consume() {
+        print("Eating \(name)!")
+        numServings -= 1
+    }
+    
+    func glaze() {
+        print("Applying glaze to \(name)!")
     }
 }
 
-var toyota = Car(name: "toyota", numPassengers: 5)
-toyota.travel(distance: 10) // prints "driving 10 miles!"
-print(toyota.isAtMaxSpeed(currentSpeed: 100)) // prints true
 
-var bmx = Bike(name: "bmx", numPassengers: 1)
-bmx.travel(distance: 2) // prints "biking 2 miles!"
-print(bmx.isAtMaxSpeed(currentSpeed: 25)) // prints false
+var chocolateCake = Cake(name: "Chocolate Cake", numServings: 8)
+chocolateCake.consume() // prints "Eating a slice of Chocolate Cake!"
+print(chocolateCake.isBaked()) // prints true
+
+var glazedDoughnut = Doughnut(name: "Glazed Doughnut", numServings: 1)
+glazedDoughnut.consume() // prints "Eating Glazed Doughnut!"
+print(glazedDoughnut.isBaked()) // prints false
+
+// protocols are useful for many reasons, but the two main ones are:
+// they enforce consistency, which is usefull for clean code
+// functions can accept the protocol instead of a specific class or struct, making them more useful **
+
+//-------------------------------------------------------------------------------------------
+
+print("\n🧠 Challenge 6: 🧠")
+print(" make a struct that follows the given protocol👇\n")
+
+protocol Desert {
+    var name: String { get }
+    var numServings: Int { get set }
+    var ingredients: [String] { get set }
+    
+    mutating func addIngredient(ingredient: String)
+    mutating func removeIngredient(ingredient: String)
+}
 
 
-//------------------------------------------------------------------------------------------- *** up to here (still need to do exercises) ***
-
+/*
+ 👇 Your new code below 👇
+*/
 
 print("\n💡 Topic 7 💡\n Extensions \n")
 
+// Extensions are useful when you want to add functionality to prexisting classes and structs
 
+extension String {
+    func isValidPastryName() -> Bool {
+        let validPastryNames: Set = ["donut", "croissant", "muffin", "cookie", "cake"]
+        return validPastryNames.contains(self)
+    }
+}
+
+print("donut".isValidPastryName()) // prints false
+print("apple".isValidPastryName()) // prints true
+
+
+print("\n🧠 Challenge 6: 🧠")
+print("Extend the dictionary class. When given a dictionary with a key value pair of the abundance of an ingredient as the key and the value as the ingredient name, print out the ingredient list ordere by abundance (descending).👇\n")
+
+let ingredients = [
+    30:"flower",
+    3:"eggs",
+    15:"sugar",
+    2:"water",
+    6:"frosting"
+]
+
+/*
+ 👇 Your new code below 👇
+*/
